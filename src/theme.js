55bd1d85b0a75921aa12ac3095ba25e8b361b438
Wanderley R. Neto <wneto_ramos@hotmail.com>
2021-02-22 19:10:22 -0300

    Criando definição de tema

---
+++ src/theme.js
@@ -0,0 +1,23 @@
+export const theme = {
+	colors: {
+		dark: '#3f3f3f',
+		light: '#e8e8e8',
+		gray: '#f3f3f3',
+		mainA: '#E8C3C2',
+		mainB: '#DE6662',
+		mainC: '#E8C3C2',
+		secundaryA: '#649C77',
+		secundaryB: '#C3E8D0',
+
+	},
+
+	metrics: {
+		width100: '100%',
+		padding: 10,
+		paddingVertical: 20,
+		margin: 10,
+		borderRadius: 7
+	},
+
+	fonts: {}
+}
